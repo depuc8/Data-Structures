c5e072a9ff3f0147617794e9b0e07e3a30fe4d89
praveendeepak <praveendeepak8koripalli@gmail.com>
2026-01-03 21:04:36 +0530

    Implemented hash-maps API

--- hash-maps/hash-maps.c
+++ hash-maps/hash-maps.c
@@ -1,21 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include "hash-maps.h"
 
-
-#define TABLE_SIZE 101
-#define NAME_SIZE 50
-#define PHONE_SIZE 16
-
-typedef struct NODE{
-
-	char name[NAME_SIZE];
-	char phone[PHONE_SIZE];
-	struct NODE *next;
-
-}NODE;
-
-NODE* Hash_Table[TABLE_SIZE];
+NODE* Hash_Table[TABLE_SIZE] = {NULL};
 
 unsigned int hash(const char *key) {
     unsigned int h = 0;
@@ -25,10 +13,49 @@ unsigned int hash(const char *key) {
     return h % TABLE_SIZE;
 }
 
-void Hash_put(char *key, char *value){
+void hash_put(const char *key, const char *value){
 
-	unsigned int index = hash(*key);
+	unsigned int index = hash(key);
 
-	if(strcmp(Hash_Table[index]->key,*key)==0)
+	NODE* current = Hash_Table[index];
 
+	while(current){
+		if(strcmp(current->key,key)==0){
+			strcpy(current->value,value);
+			return;
+		}
+		current = current->next;
+	}
+	
+	NODE* new_node = malloc(sizeof(NODE));
+	if(!new_node) return; 
+	strcpy(new_node->key,key);
+	strcpy(new_node->value,value);
+	new_node->next = Hash_Table[index];
+	Hash_Table[index] = new_node;
+}
+
+const char* hash_get(const char *key){
+
+	unsigned int index = hash(key);
+	NODE* current = Hash_Table[index];
+
+	while(current){
+		if(strcmp(current->key,key) == 0){
+			return current->value;	
+		}
+		current = current->next;
+	}
+	return NULL;	
+}
+
+void free_table(void){
+	for(int i = 0; i < TABLE_SIZE; i++){
+		NODE* current = Hash_Table[i];
+		while(current){
+			NODE* temp = current;
+			current = current->next;
+			free(temp);
+		}
+	}
 }
