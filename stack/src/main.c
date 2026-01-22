3fef2d27239987815178ef56f562068cfb965814
praveendeepak <praveendeepak8koripalli@gmail.com>
2026-01-22 23:17:28 +0530

    Added a Testing Driver

--- stack/src/main.c
+++ stack/src/main.c
@@ -1,14 +1,34 @@
+#include<stdio.h>
 #include "../inc/stack.h"
-#include <stdio.h>
+/*
+#include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
+
 #define SIZE 100
 
+typedef struct STACK{
+	uint32_t data[SIZE];
+	int32_t top;
+}STACK;
+
+void stack_init(STACK *s);
+
+void push(STACK *s ,uint32_t n);
+
+void pop(STACK *s);
+
+size_t size(const STACK *s);
+
+bool isEmpty(const STACK *s);
+
+bool isFull(const STACK *s);
+   */
 int main(){
 
-	stack s;
-	
-	stack_init(s,SIZE);
-
+	STACK s;
+	stack_init(&s);
+	push(&s,32);
 	return 0;
 
 }
-
